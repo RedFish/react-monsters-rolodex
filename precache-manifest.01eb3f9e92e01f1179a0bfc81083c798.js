9270f0f7a5c2bef7c897c354f34281fbbafc5691
Richard <richard@supermiro.com>
2020-01-05 18:03:55 +0100

    Updates

---
+++ precache-manifest.01eb3f9e92e01f1179a0bfc81083c798.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a78aedad5cb8ac96addf9037036820e1",
+    "url": "/react-monsters-rolodex/index.html"
+  },
+  {
+    "revision": "547c566077b53f9bd66e",
+    "url": "/react-monsters-rolodex/static/css/main.3223ea11.chunk.css"
+  },
+  {
+    "revision": "e2811a4a0f7aa07135f0",
+    "url": "/react-monsters-rolodex/static/js/2.ebff8ade.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-monsters-rolodex/static/js/2.ebff8ade.chunk.js.LICENSE"
+  },
+  {
+    "revision": "547c566077b53f9bd66e",
+    "url": "/react-monsters-rolodex/static/js/main.c3200789.chunk.js"
+  },
+  {
+    "revision": "832021b66317a3da9d96",
+    "url": "/react-monsters-rolodex/static/js/runtime-main.7bc6f8a8.js"
+  }
+]);
